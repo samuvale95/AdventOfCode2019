11474498d500b87c7428fcb045f30d249ebab1a5
Samuele Valente <samuelevalente@MacBook-Pro.local>
2020-11-15 16:44:23 +0100

    add second part of day_five

--- day_five.py
+++ day_five.py
@@ -1,8 +1,12 @@
 operations = {
-    1: (lambda a,b: a+b, 2, 4),
-    2: (lambda a,b: a*b, 2, 4),
-    3: (lambda: INPUT, 0, 2),
-    4: (lambda a: print(a), 1, 2),
+    1: (lambda a,b: a+b, 2, 4, True),
+    2: (lambda a,b: a*b, 2, 4, True),
+    3: (lambda: INPUT, 0, 2, True),
+    4: (lambda a: print(a), 1, 2, False),
+    5: (lambda a,b: b if a!=0 else pc+3, 2, None, False),
+    6: (lambda a,b: b if a==0 else pc+3, 2, None, False),
+    7: (lambda a,b: 1 if a<b else 0, 2, 4, True),
+    8: (lambda a,b: 1 if a==b else 0, 2, 4, True),
 }
 
 modes = {
@@ -20,9 +24,10 @@ def decode(code):
 
 
 def run(program):
-    i=0
-    while program[i] != 99 :
-        par_mode = decode(program[i])
+    global pc
+    pc=0
+    while program[pc] != 99 :
+        par_mode = decode(program[pc])
 
         res_mode = int(par_mode[0])
         par2_mode = int(par_mode[1])
@@ -32,13 +37,18 @@ def run(program):
         op_tuple = operations[opcode]
 
         if(op_tuple[1] == 2):
-            program[modes[res_mode](i+3)] = op_tuple[0](program[modes[par1_mode](i+1)], program[modes[par2_mode](i+2)])
+            if(op_tuple[3]):
+                program[modes[res_mode](pc+3)] = op_tuple[0](program[modes[par1_mode](pc+1)], program[modes[par2_mode](pc+2)])
+            else:
+                pc = op_tuple[0](program[modes[par1_mode](pc+1)], program[modes[par2_mode](pc+2)])
+                continue
         elif(op_tuple[1] == 1):
-            op_tuple[0](program[modes[par1_mode](i+1)])
+            op_tuple[0](program[modes[par1_mode](pc+1)])
         else:
-            program[modes[par1_mode](i+1)] = op_tuple[0]()
-        i+=op_tuple[2]
+            program[modes[par1_mode](pc+1)] = op_tuple[0]()
+        pc+=op_tuple[2]
 
 program = [int(i) for i in input().split(',')]
-INPUT=1
+INPUT=5
+pc=0
 run(program)
