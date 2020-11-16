f5ada6d6eaa52bdbfc77ff25d27964238a1bc11c
Samuele Valente <samuelevalente@MacBook-Pro.local>
2020-11-16 20:29:01 +0100

    add day_six second part

--- day_six.py
+++ day_six.py
@@ -3,23 +3,49 @@ def read_input(file_name):
         for line in f:
             left, right = line.split(')')
             if(orbits.get(left) is not None):
-                orbits[left].append(right.strip())
+                orbits[left.strip()].append(right.strip())
             else:
-                orbits[left] = [right.strip()]
+                orbits[left.strip()] = [right.strip()]
 
 orbits={}
 
-count=0
+def count():
+    cnt=0
+    def rec_count(orb):
+        for p in orb:
+            cnt+=1
+            if(orbits.get(p) is not None):
+                rec_count(orbits[p])
+    for _,v in orbits.items():
+        rec_count(v)
+    return cnt
 
-def rec_count(orb):
-    for p in orb:
-        global count
-        count+=1
-        if(orbits.get(p) is not None):
-            rec_count(orbits[p])
+def search(node):
+    for k,v in orbits.items():
+        if(node in v):
+            return k
+
+def find_path_to_root(node, path=[]):
+    path.append(node)
+    for k,v in orbits.items():
+        if(node in v):
+            find_path_to_root(k, path)
+    return path
+
 
 
 read_input('in.txt')
-for k,v in orbits.items():
-    rec_count(v)
-print(count)
+you_path = find_path_to_root(search('YOU'),[])
+san_path = find_path_to_root(search('SAN'),[])
+
+
+
+is_find_path=False
+while not is_find_path:
+    if(you_path[-2]==san_path[-2]):
+        you_path.pop()
+        san_path.pop()
+    else:
+        print(len(set(you_path+san_path))-1)
+        is_find_path=True
+
